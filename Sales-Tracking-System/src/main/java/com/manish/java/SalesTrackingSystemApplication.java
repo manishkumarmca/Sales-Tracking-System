f13208d8296edb2fb87aab176e1eb86018e79303
Manish kumar <manishkumarmca131@gmail.com>
2022-11-17 12:42:32 +0530

    "Starting a Sales Tracking Project"

---
+++ Sales-Tracking-System/src/main/java/com/manish/java/SalesTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.manish.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTrackingSystemApplication.class, args);
+	}
+
+}
